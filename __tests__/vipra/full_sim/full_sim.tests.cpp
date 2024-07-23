0fab5774b6289a0c54fef0164097b47f01619e43
Rolland Goodenough <rtg13@students.uwf.edu>
2024-07-23 18:24:09 -0400

    updating tests to match

--- __tests__/vipra/full_sim/full_sim.tests.cpp
+++ __tests__/vipra/full_sim/full_sim.tests.cpp
@@ -4,15 +4,22 @@
 #include "model/calm_model/calm_model.hpp"
 #include "vipra.hpp"
 
-TEST(VipraFullSim, FullSim) {
-  auto sim =
-      VIPRA::simulation(CALM::Model{}, VIPRA::Goals::AStar{},
-                        VIPRA::Pedestrians::Grid{
-                            VIPRA::Input::JSON{"../maps/pedestrians/a320/a320_144_pedestrians.json"},
-                        },
-                        VIPRA::Module::Output{VIPRA::Output::Trajectories::JSON{}},
-                        VIPRA::Module::Map{VIPRA::Input::JSON{"../maps/obstacles/a320/a320_polygons.json"},
-                                           VIPRA::Obstacles::QuadTree{}});
+TEST(VipraFullSim, FullSim)
+{
+  auto sim = VIPRA::simulation(
+      CALM::Model{},
+      VIPRA::Goals::AStar{},
+      VIPRA::Pedestrians::Grid{},
+      VIPRA::CoordModules::Output{
+        VIPRA::Output::Trajectories::JSON{}
+      },
+      VIPRA::Obstacles::QuadTree{}
+  );
 
-  sim(VIPRA::Parameters{VIPRA::Input::JSON{"vipra/full_sim/module_params.json"}});
+  // Run the simulation with the module parameters in 'module_params.json'
+  sim(
+    VIPRA::Input::JSON{"maps/pedestrians/a320/a320_144_pedestrians.json"},
+    VIPRA::Input::JSON{"maps/obstacles/a320/a320_polygons.json"},
+    VIPRA::Parameters{VIPRA::Input::JSON{"module_params.json"}}
+    );
 }
